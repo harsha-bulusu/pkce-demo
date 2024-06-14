d738ac38bf3f78802e0fd6860af8a6356a7814db
Sri Harsha <sriharsha@Sris-MacBook-Pro.local>
2024-06-14 10:20:15 +0530

    adding projects:

---
+++ pkce-backend-resource-server/src/main/java/com/harsha/app/PkceBackendResourceServerApplication.java
@@ -0,0 +1,13 @@
+package com.harsha.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PkceBackendResourceServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PkceBackendResourceServerApplication.class, args);
+	}
+
+}
